8e735c32f728a5253cc24c85a7e6797f29516cce
ainlorn <ap4khomov@gmail.com>
2025-09-21 08:40:50 +0500

    display timer in matching

--- MFGTweaks/Tweaks/TimerFreeze.cs
+++ MFGTweaks/Tweaks/TimerFreeze.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using UnityEngine.SceneManagement;
 
 namespace MFGTweaks.Tweaks;
 
@@ -11,23 +12,31 @@ internal class TimerFreeze : BaseTweak
     {
         Harmony.PatchAll(typeof(TimerFreeze));
     }
+    
+    private static bool IsMatching => SceneManager.GetActiveScene().name == "Matching";
 
     [HarmonyPostfix, HarmonyPatch(typeof(CountDownTimer), nameof(CountDownTimer.Start))]
     private static void Hook_CountDownTimer_Start(CountDownTimer __instance)
     {
+        if (IsMatching)
+            return;
+        
         __instance.m_disp = false;
     }
     
     [HarmonyPrefix, HarmonyPatch(typeof(CountDownTimer), nameof(CountDownTimer.Update))]
     private static bool Hook_CountDownTimer_Update()
     {
-        return false;
+        return IsMatching;
     }
     
     [HarmonyPrefix, HarmonyPatch(typeof(CountDownTimer), nameof(CountDownTimer.SetDisp))]
     private static void Hook_CountDownTimer_SetDisp(ref bool disp)
     {
+        if (IsMatching)
+            return;
+        
         disp = false;
     }
-    
+
 }
